49cc8de665ab2353757c76493b5bd1308077f605
kakkki <kakki4062@gmail.com>
2021-12-10 15:44:17 +0900

    Initial Commit

---
+++ SwiftUICombineAndData/SwiftUICombineAndDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineAndDataApp.swift
+//  SwiftUICombineAndData
+//
+//  Created by Atsuki Kakehi on 2021/12/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineAndDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
